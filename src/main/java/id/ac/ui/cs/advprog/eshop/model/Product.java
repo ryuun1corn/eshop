5adebd616e086c3689a64cd94e3010088193fa2d
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-02-10 16:20:18 +0700

    refactor: change productId to UUID type

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,15 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter @Setter
 public class Product {
-    private String productId;
+    public Product() {
+        productId = UUID.randomUUID();
+    }
+
+    private final UUID productId;
     private String productName;
     private int productQuantity;
 }

623845b6a95e911151cd0060db51f444c2655a3c
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-02-17 10:24:18 +0700

    chore: add unit test for homepage

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomepageController.class)
+public class HomepageControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void homePage_ShouldReturnHomepageView() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Homepage"));
+    }
+}
+
+

1ca16d8a5367c3cda9bc3dce1373325d275ccbb8
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-02-17 10:53:41 +0700

    refactor: modify HomepageControllerTest to use Mockito's ExtendWith instead of Mvc tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
@@ -1,18 +1,30 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(HomepageController.class)
+@ExtendWith(MockitoExtension.class)
 public class HomepageControllerTest {
-    @Autowired
     private MockMvc mockMvc;
+
+    @InjectMocks
+    private HomepageController homepageController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(homepageController).build();
+    }
 
     @Test
     void homePage_ShouldReturnHomepageView() throws Exception {
